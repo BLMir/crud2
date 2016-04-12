3d9397afb0a10b04afc1b451129daf133fc97e52
biel mir <iambielmir@gmail.com>
2016-04-12 14:23:46 +0200

    implement bootstrap & jquery with bower

--- src/AppBundle/Controller/userController.php
+++ src/AppBundle/Controller/userController.php
@@ -14,8 +14,6 @@ class userController extends Controller
     public function indexAction(Request $request)
     {
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+        return $this->render('AppBundle:user:users.html.twig');
     }
 }
