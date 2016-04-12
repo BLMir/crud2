c25ab94890c51a077f0a320d2fc11b04bab3e01f
biel mir <iambielmir@gmail.com>
2016-04-12 13:02:29 +0200

    move indexAction from default controller to UserController

--- src/AppBundle/Controller/userController.php
+++ src/AppBundle/Controller/userController.php
@@ -2,9 +2,20 @@
 
 namespace AppBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class userController extends Controller
 {
+	/**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', array(
+            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
 }
