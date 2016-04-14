693c123236b70339a88a6db5f0148d264b287a00
biel mir <iambielmir@gmail.com>
2016-04-14 13:23:34 +0200

    inserted new property into register form ROLE

---
+++ src/AppBundle/Form/RegistrationType.php
@@ -0,0 +1,33 @@
+<?php
+namespace AppBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('role','choice',array('choices' => array('ROLE_PROFESSIONAL' => 'Professional', 'ROLE_USER' => 'User'), 'placeholder' => 'Select a role'))
+;
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getRole()
+    {
+        return $this->getBlockPrefix();
+    }
+}
